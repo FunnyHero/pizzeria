a058764b5aa7896080986c757e17908cbae8b18a
Igor Shemarov <hgdrtttr@gmail.com>
2021-05-25 01:52:57 +0300

    Create Order model

---
+++ src/main/java/com/epam/student/practice/model/Order.java
@@ -0,0 +1,42 @@
+package com.epam.student.practice.model;
+
+import com.epam.student.practice.model.customer.Customer;
+import com.epam.student.practice.model.payment.Payment;
+
+public class Order {
+    private Cart cart;
+
+    private Customer customer;
+
+    private Payment payment;
+
+    public Order(Cart cart, Customer customer, Payment payment) {
+        this.cart = cart;
+        this.customer = customer;
+        this.payment = payment;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+}
