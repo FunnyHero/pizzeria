3969352c5ce92b851dd559b3c2cca1ae24d0314a
Igor Shemarov <hgdrtttr@gmail.com>
2021-05-25 00:46:28 +0300

    Create Drink model

---
+++ src/main/java/com/epam/student/practice/model/goods/Drink.java
@@ -0,0 +1,18 @@
+package com.epam.student.practice.model.goods;
+
+public class Drink extends Goods {
+    private double volume;
+
+    public Drink(String name, NutritionFacts nutritionFacts, double cost, int quantity, double volume) {
+        super(name, nutritionFacts, cost, quantity);
+        this.volume = volume;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+}
