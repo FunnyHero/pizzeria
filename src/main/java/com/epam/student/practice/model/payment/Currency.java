438c05b2ff65609dcd358ff572c6bddb0e08df65
Igor Shemarov <hgdrtttr@gmail.com>
2021-05-25 01:52:42 +0300

    Create Payment model

---
+++ src/main/java/com/epam/student/practice/model/payment/Currency.java
@@ -0,0 +1,5 @@
+package com.epam.student.practice.model.payment;
+
+public enum Currency {
+    RUSSIAN_RUBLE, UKRAINIAN_HRYVNIA, US_DOLLAR, EURO
+}
---
+++ src/main/java/com/epam/student/practice/model/payment/PaymentType.java
@@ -0,0 +1,5 @@
+package com.epam.student.practice.model.payment;
+
+public enum PaymentType {
+    CASH, CARD
+}
