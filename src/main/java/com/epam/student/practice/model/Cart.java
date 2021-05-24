15c617ced1d702c4775f9b918466a8adde3a59d7
Igor Shemarov <hgdrtttr@gmail.com>
2021-05-25 01:13:44 +0300

    Create Cart model

---
+++ src/main/java/com/epam/student/practice/model/Cart.java
@@ -0,0 +1,61 @@
+package com.epam.student.practice.model;
+
+import com.epam.student.practice.model.goods.Drink;
+import com.epam.student.practice.model.goods.Goods;
+import com.epam.student.practice.model.goods.Pizza;
+
+import java.util.List;
+
+public class Cart {
+    private List<Goods> goodsList;
+
+    public Cart(List<Goods> goodsList) {
+        this.goodsList = goodsList;
+    }
+
+    public List<Goods> getGoodsList() {
+        return goodsList;
+    }
+
+    public void setGoodsList(List<Goods> goodsList) {
+        this.goodsList = goodsList;
+    }
+
+    public void addGoods(Goods goods){
+        this.goodsList.add(goods);
+    }
+
+    public boolean isLessThanFivePizzas(){
+        int pizzasCounter = 0;
+
+        for (Goods goods : goodsList) {
+            if (goods instanceof Pizza){
+                pizzasCounter++;
+            }
+        }
+
+        return pizzasCounter < 5;
+    }
+
+    public boolean isLessThanFourDrinks(){
+        int drinksCounter = 0;
+
+        for (Goods goods : goodsList) {
+            if (goods instanceof Drink){
+                drinksCounter++;
+            }
+        }
+
+        return drinksCounter < 4;
+    }
+
+    public double getTotalCost(){
+        int totalCost = 0;
+
+        for (Goods goods : goodsList) {
+            totalCost += goods.calculateCost();
+        }
+
+        return totalCost;
+    }
+}
