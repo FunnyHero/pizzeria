c22292332487f541aa2926cc8bcfe1f4432fbe79
Igor Shemarov <hgdrtttr@gmail.com>
2021-05-28 02:53:47 +0300

    Upd repo and controller

--- src/main/java/com/epam/student/practice/controller/SampleController.java
+++ src/main/java/com/epam/student/practice/controller/SampleController.java
@@ -1,73 +1,45 @@
 package com.epam.student.practice.controller;
 
 import com.epam.student.practice.dao.Repository;
+import com.epam.student.practice.dto.DeliveryDTO;
 import com.epam.student.practice.dto.OrderDTO;
 import com.epam.student.practice.model.Cart;
 import com.epam.student.practice.model.Order;
-import com.epam.student.practice.model.customer.Customer;
 import com.epam.student.practice.model.goods.Goods;
-import com.epam.student.practice.model.payment.Payment;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.UUID;
 
 @RestController
 public class SampleController {
 
-    Repository repository = new Repository();
-
-    @GetMapping("/home")
-    public String home(){
-        return "home";
-    }
-
-    @GetMapping("/about")
-    public String about(){
-        return "about";
-    }
-
-    @GetMapping("/inst")
-    public RedirectView inst(){
-        return new RedirectView("https://www.instagram.com/");
-    }
-
-    @GetMapping("/location")
-    public RedirectView location(){
-        return new RedirectView("https://yandex.ru/maps/?um=constructor%3Ab0eaa97fc06386ffbd3a69b13a7b7a467494e52626da0eab5a71506d78c6053d&source=https://yandex.ru/maps/?um=constructor%3Ab0eaa97fc06386ffbd3a69b13a7b7a467494e52626da0eab5a71506d78c6053d&source=constructorLink");
-    }
-
-    @GetMapping("/cart")
-    public String cart(){
-        return "cart";
-    }
-
     @GetMapping("/getGoodsInfo")
-    public Goods getGoodsInfo(@RequestParam(value = "name") String name) {
-        return repository.findByName(name);
+    public Collection<Goods> getGoodsInfo(@RequestBody String name) {
+        return Repository.getInstance().findByName(name);
     }
 
     @GetMapping("/getMenu")
-    public ArrayList<Goods> getMenu() {
-        return repository.findAllGoods();
+    public Collection<Goods> getMenu() {
+        return Repository.getInstance().findAllGoods();
     }
 
-    @GetMapping("/createOrder")
-    public String createOrder() {
-        return "createOrder";
+    @GetMapping
+    public Goods getGoodsById(@RequestBody UUID id){
+        return Repository.getInstance().findById(id);
     }
 
     @PostMapping("/createOrder")
-    public long createOrder(@RequestBody OrderDTO orderDTO, Customer customer, Payment payment) {
-        List<Goods> goodsList = new ArrayList<>();
+    public DeliveryDTO createOrder(@RequestBody Collection<OrderDTO> orderDTO) {
+        Cart cart = new Cart();
 
-        for (String goodsName : orderDTO.getGoodsNameList()) {
-            goodsList.add(repository.findByName(goodsName));
+        for (OrderDTO oDTO : orderDTO) {
+            Goods goods = Repository.getInstance().findById(oDTO.getGoodsID());
+            goods.setQuantity(oDTO.getQuantity());
+            cart.addGoods(goods);
         }
-        
-        Order order = new Order(new Cart(goodsList), customer, payment);
-        return 0;
+
+        return new DeliveryDTO(Repository.getInstance().createOrder(new Order(cart, null, null)));//TODO
     }
 }
 
--- src/main/java/com/epam/student/practice/dao/Repository.java
+++ src/main/java/com/epam/student/practice/dao/Repository.java
@@ -1,5 +1,6 @@
 package com.epam.student.practice.dao;
 
+import com.epam.student.practice.model.Order;
 import com.epam.student.practice.model.customer.Customer;
 import com.epam.student.practice.model.goods.Drink;
 import com.epam.student.practice.model.goods.Goods;
@@ -7,40 +8,72 @@ import com.epam.student.practice.model.goods.NutritionFacts;
 import com.epam.student.practice.model.goods.Pizza;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.UUID;
 
-public class Repository {
+public final class Repository {
 
-    private ArrayList<Goods> goodsList;
+    private static Repository repository;
 
-    public Repository() {
-        this.goodsList = new ArrayList<>();
-        goodsList.add(new Pizza("Мясная", new NutritionFacts(252, 11.1, 9.5, 29.1),
+    private Collection<Goods> goodsList;
+
+    private Collection<Order> orderList;
+
+    private void initGoodsList(){
+        goodsList.add(new Pizza(UUID.randomUUID(),"Мясная", new NutritionFacts(252, 11.1, 9.5, 29.1),
                 175, 1, 660, 30,
                 "Цыпленок, ветчина, пикантная пепперони, острая чоризо, моцарелла, томатный соус"));
-        goodsList.add(new Pizza("Фирменная", new NutritionFacts(297, 11.1, 15.1, 27.3),
+        goodsList.add(new Pizza(UUID.randomUUID(),"Фирменная", new NutritionFacts(297, 11.1, 15.1, 27.3),
                 275, 1, 640, 30,
                 "Бекон, митболы из говядины, пикантная пепперони, моцарелла, томаты черри, шампиньоны, сладкий перец, красный лук, чеснок, томатный соус"));
-        goodsList.add(new Pizza("Четыре сыра", new NutritionFacts(301.9, 12.7, 13.1, 31.3),
+        goodsList.add(new Pizza(UUID.randomUUID(),"Четыре сыра", new NutritionFacts(301.9, 12.7, 13.1, 31.3),
                 1, 150, 0, 30,
                 "Сыр блю чиз, сыры чеддер и пармезан, моцарелла, соус альфредо"));
-        goodsList.add(new Pizza("Закрытая с грибами", new NutritionFacts(357, 8, 7, 19),
+        goodsList.add(new Pizza(UUID.randomUUID(),"Закрытая с грибами", new NutritionFacts(357, 8, 7, 19),
                 1, 300, 500, 30,
                 "Шампиньоны, помидоры, маслины, шпинат, сыр чеддер"));
 
-        goodsList.add(new Drink("Пепси", new NutritionFacts(321,2,0,13),
+        goodsList.add(new Drink(UUID.randomUUID(),"Пепси", new NutritionFacts(321,2,0,13),
                 50,1,0.75));
-        goodsList.add(new Drink("Морс", new NutritionFacts(213,1.2,0.4,12),
+        goodsList.add(new Drink(UUID.randomUUID(),"Морс", new NutritionFacts(213,1.2,0.4,12),
                 70,1,0.75));
-        goodsList.add(new Drink("Вода", new NutritionFacts(123,0,0,0),
+        goodsList.add(new Drink(UUID.randomUUID(),"Вода", new NutritionFacts(123,0,0,0),
                 40,1,0.75));
     }
 
-    public ArrayList<Goods> findAllGoods() {
-        return this.goodsList;
+    private Repository() {
+        initGoodsList();
     }
 
-    public Goods findByName(String s) {
-        return this.goodsList.stream().filter(goods -> goods.getName().equals(s)).findFirst().get();
+    public static Repository getInstance() {
+        if (repository == null){
+            repository = new Repository();
+        }
+        return repository;
     }
 
+    public Collection<Goods> findAllGoods() {
+        return goodsList;
+    }
+
+    public Collection<Goods> findByName(String s) {
+        Collection<Goods> filteredByName = new ArrayList<>();
+
+        goodsList.forEach(goods -> {
+            if (goods.getName().contains(s)){
+                filteredByName.add(goods);
+            }
+        });
+        return filteredByName;
+    }
+
+    public Goods findById(UUID id){
+        return goodsList.stream().filter(goods -> goods.getId().equals(id)).findFirst().orElse(null);
+    }
+
+    public UUID createOrder(Order order){
+        order.setUuid(UUID.randomUUID());
+        orderList.add(order);
+        return order.getUuid();
+    }
 }
