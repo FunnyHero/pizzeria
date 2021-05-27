28f956c5d4c58a0208361e8131b2894e3c0c423a
Igor Shemarov <hgdrtttr@gmail.com>
2021-05-28 02:54:42 +0300

    Added delivery DTO and upd order DTO

---
+++ src/main/java/com/epam/student/practice/dto/DeliveryDTO.java
@@ -0,0 +1,19 @@
+package com.epam.student.practice.dto;
+
+import java.util.UUID;
+
+public class DeliveryDTO {
+    UUID orderID;
+
+    public DeliveryDTO(UUID orderID) {
+        this.orderID = orderID;
+    }
+
+    public UUID getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(UUID orderID) {
+        this.orderID = orderID;
+    }
+}
--- src/main/java/com/epam/student/practice/dto/OrderDTO.java
+++ src/main/java/com/epam/student/practice/dto/OrderDTO.java
@@ -1,24 +1,46 @@
 package com.epam.student.practice.dto;
 
-import java.util.List;
 import java.util.UUID;
 
 public class OrderDTO {
+    UUID goodsID;
 
-    private List<String> goodsNameList;
+    int quantity;
 
-    public OrderDTO() {
+    public OrderDTO(UUID goodsID, int quantity) {
+        this.goodsID = goodsID;
+        this.quantity = quantity;
     }
 
-    public OrderDTO(List<String> goodsNameList) {
-        this.goodsNameList = goodsNameList;
+    public UUID getGoodsID() {
+        return goodsID;
     }
 
-    public List<String> getGoodsNameList() {
-        return goodsNameList;
+    public void setGoodsID(UUID goodsID) {
+        this.goodsID = goodsID;
     }
 
-    public void setGoodsNameList(List<String> goodsNameList) {
-        this.goodsNameList = goodsNameList;
+    public int getQuantity() {
+        return quantity;
     }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+//    private List<String> goodsNameList;
+//
+//    public OrderDTO() {
+//    }
+//
+//    public OrderDTO(List<String> goodsNameList) {
+//        this.goodsNameList = goodsNameList;
+//    }
+//
+//    public List<String> getGoodsNameList() {
+//        return goodsNameList;
+//    }
+//
+//    public void setGoodsNameList(List<String> goodsNameList) {
+//        this.goodsNameList = goodsNameList;
+//    }
 }
